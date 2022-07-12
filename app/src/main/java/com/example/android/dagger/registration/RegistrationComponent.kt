ab6758c03e61b4892e9e6ee9fdafca208f4751e8
WhoisAbel <whoisabel1996@gmail.com>
2022-07-12 14:49:57 +0430

    7-Scoping Subcomponent
    - When a type is marked with a scope annotation, it can only be used by Components that are annotated with the same scope.
    - When a Component is marked with a scope annotation, it can only provide types with that annotation or types that have no annotation.
    - A subcomponent cannot use a scope annotation used by one of its parent Components.
    - If you noticed, the variable registrationComponent is not annotated with @Inject because we're not expecting that variable to be provided by Dagger.

--- app/src/main/java/com/example/android/dagger/registration/RegistrationComponent.kt
+++ app/src/main/java/com/example/android/dagger/registration/RegistrationComponent.kt
@@ -1,11 +1,13 @@
 package com.example.android.dagger.registration
 
+import com.example.android.dagger.di.ActivityScope
 import com.example.android.dagger.registration.enterdetails.EnterDetailsFragment
 import com.example.android.dagger.registration.termsandconditions.TermsAndConditionsFragment
 import dagger.Component
 import dagger.Subcomponent
 
 @Subcomponent
+@ActivityScope
 interface RegistrationComponent {
 
     @Subcomponent.Factory
